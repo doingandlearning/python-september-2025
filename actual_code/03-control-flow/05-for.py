275386a1e533273f2509a4ef9b592d5bcf9289b6
Kevin Cunningham <kevin@kevincunningham.co.uk>
2025-09-22 12:41:18 +0100

    adds for

---
+++ actual_code/03-control-flow/05-for.py
@@ -0,0 +1,10 @@
+
+# we know how many ... 
+for age in range(5):  # 0, 1, 2, 3, 4
+  print(age)
+
+for letter in "thank goodness it's almost lunch":
+  if letter == " ":
+    continue 
+  print(letter)
+
