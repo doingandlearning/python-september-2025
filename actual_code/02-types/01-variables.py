1cdf673b48ffa9b88dddb10a23d83537b8594563
Kevin Cunningham <kevin@kevincunningham.co.uk>
2025-09-22 10:47:06 +0100

    adds current code

---
+++ actual_code/02-types/01-variables.py
@@ -0,0 +1,20 @@
+first_and_last_name = "Kevin Cunningham"
+
+print('Kevin Cunningham')
+print(first_and_last_name)       # value - Kevin Cunningham
+print(type(first_and_last_name)) # type - str  (string)
+
+print(first_and_last_name.center(100))
+print(first_and_last_name.upper())
+print(first_and_last_name.lower())
+
+print(len(first_and_last_name))  # find the length
+
+# String Concatentation -> join together strings
+print("Hello, " + first_and_last_name + ", how are you today?")
+print(f"Hello, {first_and_last_name}, how are you today?")
+print(f'1 + 1 = {1 + 1}')
+
+print("""1. This is the first line.
+2. This is the second line.
+""")
